603881f405d8da24fc10cf4e5b8e02dccfbd9c54
Guido Castro <guid.castr@gmail.com>
2023-09-14 03:05:33 +0200

    add lazygit to toggleterm and std floating term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,13 +1,51 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-    opts = {
-		close_on_exit = true,
-		hide_numbers = true,
-		insert_mappings = true,
-		autochdir = false,
-		size =15,
-		direction = "horizontal",
-        open_mapping=[[<c-\>]]
-    },
+	event = "VeryLazy",
+	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+			float_opts = {
+				border = "single",
+				-- width = <cambiar int>,
+				-- height = <cambiar int>,
+				winblend = 8,
+				zindex = 20,
+			},
+		})
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+
+		require("toggleterm").setup({
+			close_on_exit = true,
+			hide_numbers = true,
+			insert_mappings = true,
+			autochdir = false,
+			shading_factor = 20,
+			size = 20,
+			-- direction = "horizontal",
+			direction = "float",
+			-- direction = "vertical",
+			open_mapping = [[<c-\>]],
+			float_opts = {
+				-- The border key is *almost* the same as 'nvim_open_win'
+				-- see :h nvim_open_win for details on borders however
+				-- the 'curved' border is a custom border type
+				-- not natively supported but implemented in this plugin.
+				border = "curved",
+				-- like `size`, width and height can be a number or function which is passed the current terminal
+				-- width = <value>,
+				-- height = <value>,
+				winblend = 3,
+				zindex = 10,
+			},
+		})
+	end,
 }
