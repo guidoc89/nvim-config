3e357a3115f53953dec2fe7e5c79e81b58df4acd
guido <guid.castr@gmail.com>
2024-10-10 17:05:44 +0200

    feature(neogit): add lazy load

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,14 +1,11 @@
--- return {}
-
--- return {}
-
 return {
-    'NeogitOrg/neogit',
-     dependencies = 'nvim-lua/plenary.nvim',
-    config = function()
-        require("neogit").setup{}
-    end,
+	"NeogitOrg/neogit",
+	cmd = "Neogit",
+	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
 		{ "<leader>gy", "<cmd>Neogit kind=vsplit<cr>", mode = { "n" }, desc="Neogit open vertical split" },
 	},
+	config = function()
+		require("neogit").setup({})
+	end,
 }
