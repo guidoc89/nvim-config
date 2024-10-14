2fba2882bfafd5dfbc3c88b3576fde66591e4633
guido <guid.castr@gmail.com>
2024-10-14 11:52:32 +0200

    refactor(gitsigns): keymaps dont popup the noice cmdline anymore

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,30 +1,38 @@
-
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup{}
-    end,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        signs = {
-            add = { text = "▎" },
-            change = { text = "▎" },
-            delete = { text = "" },
-            topdelete = { text = "" },
-            changedelete = { text = "▎" },
-            untracked = { text = "▎" },
-        },
-    },
-    keys = {
-        {"<leader>ghc", ":Gitsigns blame_line<CR>", mode={"n"}, desc="Gitsigns blame line"},
-        {"<leader>glb", ":Gitsigns toggle_current_line_blame<CR>", mode={"n"}, desc="Gitsigns toggle blame current line"},
-        {"]g", ":Gitsigns next_hunk<CR>", mode={"n"}, desc="Gitsigns next hunk"},
-        {"[g", ":Gitsigns prev_hunk<CR>", mode={"n"}, desc="Gitsigns prev hunk"},
-        {"<leader>ghd", ":Gitsigns diffthis<CR>", mode={"n"}, desc="Gitsigns diffthis"},
-        {"<leader>ghp", ":Gitsigns preview_hunk<CR>", mode={"n"}, desc="Gitsigns preview hunk"},
-        {"<leader>ghi", ":Gitsigns preview_hunk_inline<CR>", mode={"n"}, desc="Gitsigns preview hunk inline"},
-        {"<leader>gha", ":Gitsigns stage_hunk<CR>", mode={"n"}, desc="Gitsigns stage hunk"},
-        {"<leader>ghr", ":Gitsigns reset_hunk<CR>", mode={"n"}, desc="Gitsigns reset hunk"},
-        {"<leader>ghu", ":Gitsigns undo_stage_hunk<CR>", mode={"n"}, desc="Gitsigns undo stage hunk"},
-    },
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				map("n", "<leader>gha", gitsigns.stage_hunk, { desc = "Stage hunk" })
+				map("n", "<leader>ghr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+				map("n", "<leader>ghu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+				map("n", "<leader>ghc", gitsigns.blame_line, { desc = "Blame line" })
+				map("n", "<leader>glb", gitsigns.toggle_current_line_blame, { desc = "Toggle curr line blame" })
+				map("n", "<leader>ghp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+				map("n", "<leader>ghi", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+				map("n", "<leader>ghd", gitsigns.diffthis, { desc = "Gitsigns diff buffer" })
+				map("n", "[g", gitsigns.prev_hunk, { desc = "Prev hunk" })
+				map("n", "]g", gitsigns.next_hunk, { desc = "Next hunk" })
+			end,
+		})
+	end,
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
+		},
+	},
 }
