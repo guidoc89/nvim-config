8a635afee5136d7da89db4e37c5bfdd2c862c4d7
guido <guid.castr@gmail.com>
2025-01-16 22:22:55 +0100

    clean(gh-saga-symbolsoutline-whichkey): remove, not used atm

--- lua/guido/telescope.lua
+++ lua/guido/telescope.lua
@@ -82,7 +82,7 @@ telescope.setup({
 				["<C-q>"] = open_with_trouble,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 				["<C-l>"] = actions.complete_tag,
-				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+				["<C-_>"] = actions.which_key,
 			},
 			i = {
 				["<leader>q"] = actions.close,
@@ -105,7 +105,7 @@ telescope.setup({
 				["<C-q>"] = open_with_trouble,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 				["<C-l>"] = actions.complete_tag,
-				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+				["<C-_>"] = actions.which_key,
 			},
 		},
 	},
