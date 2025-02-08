bcb19fb50a74bca35ee43def98c213553989d1bc
guido <guid.castr@gmail.com>
2025-02-08 16:33:40 +0100

    clean(null-ls): remove unused formatting sources

--- lua/guido/lsp.lua
+++ lua/guido/lsp.lua
@@ -335,10 +335,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.djhtml,
-		null_ls.builtins.formatting.djlint,
 		null_ls.builtins.formatting.rustfmt,
-		-- null_ls.builtins.formatting.black.with({ extra_args = {"--diff", "--check", "--color" }}),
 	},
 })
 
