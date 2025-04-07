5e8d202936b34f46268955a83dbc620eb58a7ee0
guido <guid.castr@gmail.com>
2025-04-07 12:03:38 +0200

    feat(neogit): full buffer keymap

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
 		{ "<leader>gy", "<cmd>Neogit kind=vsplit<cr>", mode = { "n" }, desc="Neogit open vertical split" },
+		{ "<leader>gY", "<cmd>Neogit<cr> ", mode = { "n" }, desc="Neogit" },
 	},
 	config = function()
 		require("neogit").setup({})
