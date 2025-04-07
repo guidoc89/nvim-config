ad56c4d11c2120f3ee18e142549317588a784072
guido <guid.castr@gmail.com>
2025-04-07 12:38:00 +0200

    feat(bufferline): add mini icons dependency and change separator style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
   "akinsho/bufferline.nvim",
     dependencies = {
         "echasnovski/mini.bufremove",
+        "echasnovski/mini.icons",
     },
   event = "VeryLazy",
   keys = {
@@ -16,6 +17,7 @@ return {
       -- stylua: ignore
       right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
       diagnostics = "nvim_lsp",
+      separator_style = "slope",
       always_show_bufferline = false,
       diagnostics_indicator = function(_, _, diag)
         local icons = require("utils").icons.diagnostics
