ae743750deb172f52be7b554a519fa8ac01e802e
guido <guid.castr@gmail.com>
2025-05-02 19:27:17 +0200

    feat(flash): add remote keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,5 +5,6 @@ return {
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
   },
 }
