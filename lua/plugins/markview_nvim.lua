ec1c1e097ef5e26346e021b5d96e765e99ef8f22
Guido Castro <guid.castr@gmail.com>
2025-10-14 20:05:19 +0200

    feat(markview): add keymaps and not-enabled as default

--- lua/plugins/markview_nvim.lua
+++ lua/plugins/markview_nvim.lua
@@ -2,4 +2,17 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false,
     priority = 49,
-};
+    dependencies = {
+        "saghen/blink.cmp",
+    },
+    keys = {
+        { "<leader>mo", "<cmd>Markview splitToggle<cr>", mode = {"n"}, desc = "Markview split view toggle"},
+        { "<leader>mt", "<cmd>Markview toggle<cr>", mode = {"n"}, desc = "Markview toggle"},
+    },
+    opts = {
+        preview = {
+            enable = false,
+            icon_provider = "mini",
+        },
+    },
+}
